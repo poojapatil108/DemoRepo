fa3dee97d7d506e9973169b338af9f0c923ebd6d
poojapatil108 <121961165+poojapatil108@users.noreply.github.com>
2023-01-08 19:18:21 +0530

    Basic Steps
    Hi Team,
    
    I have created framework structure, go ahead and clone it and start with your work.

---
+++ DalalStreetProject/src/com/mypackage/MyClass.java
@@ -0,0 +1,14 @@
+package com.mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Maven Dependency add");
+		System.out.println("Extent Rport add");
+		System.out.println("Log4J Add");
+		System.out.println("Basic Stuff");
+	}
+
+}
